4b8664354cfa86a713846e02db6332a463cbd795
sanjeevyg <yogi.sjv@gmail.com>
2021-11-02 23:20:59 -0400

    routes created

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Pages/Home.js';
+import Project from './Pages/Home.js';
+import About from './Pages/Home.js';
+import Contact from './Pages/Home.js';
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/" render={(routerProps) => <Home {...routerProps}/>}/>
+      <Route exact path="/projects" render={(routerProps) => <Project {...routerProps}/>}/>
+      <Route exact path="/about" render={(routerProps) => <About {...routerProps}/>}/>
+      <Route exact path="/contact" render={(routerProps) => <Contact {...routerProps}/>}/>
+    </Switch>
+  );
+}
+
+export default App;
---
+++ src/Pages/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import { Route } from 'react-router';
+
+export default function Contact() {
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+}
