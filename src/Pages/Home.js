959954e9ddf463a7fbf50efb606434b9b2deaef8
sanjeevyg <yogi.sjv@gmail.com>
2021-11-08 23:45:13 -0500

    monday

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { FaBars, FaAngleDoubleLeft} from 'react-icons/fa';
 
 
 export default function Home(props) {
     
-
     const [state, setToggleState] = useState(false)
 
     const handleToggle = () => {
         setToggleState(!state)
     }
 
-
-
-
-   
-    
     return (
         <div class="home">
             <section>
                 <div onClick={handleToggle} className= {state ? "toggleActive" : "toggle"}>
-                    {state ? <FaAngleDoubleLeft  size={36}/> : <FaBars size={36}/>}
+                    <div> 
+                        {state ? <FaAngleDoubleLeft size={36}/> : <FaBars size={36}/>}
+                        {state ? 
+                        <div> 
+                            <ul> Projects </ul>
+                            <ul> Contact </ul>
+                        </div> : 
+                        <div>test</div>
+                        }
+                    </div>
+                  
                 </div>
             </section>
         </div>
