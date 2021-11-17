91dd9b1e17411279638f6f1fe5e803e6cefa74b3
sanjeevyg <yogi.sjv@gmail.com>
2021-11-17 00:19:18 -0500

    home page 17th Nov

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,6 +5,7 @@ import SvgBooks from '../../icons/Components/Books';
 import SvgWatch from '../../icons/Components/Watch';
 import SvgProgrammer from '../../icons/Components/Programmer';
 import { AiOutlineMenu, AiOutlineMenuFold, AiFillInstagram, AiFillTwitterCircle, AiFillGithub, AiFillProject} from 'react-icons/ai';
+import { GiBarbedSun} from 'react-icons/gi';
 
 
 
@@ -13,23 +14,48 @@ import { AiOutlineMenu, AiOutlineMenuFold, AiFillInstagram, AiFillTwitterCircle,
 export default function Home(props) {
     return (
         <div className="home">
+            <div className="background">
+                
+                <button>Feedback</button>
+            </div>
             <section className="header">
                 <div id="name">
-                    Sanjeev Yogi
+                    <div className="star">
+                        <a href="#"><GiBarbedSun size={55}/></a>
+                    </div>
+
+                    <div>SANJEEV YOGI</div>
+                    <span>PROJECTS</span>
+                    <span>ABOUT</span>
+                    <span>CONTACT</span>
+                    <span>BLOGS</span>
                 </div>
-                <div id="letsTalk">
-                    <button>
-                        Let's Talk
-                    </button>
+                <div className="letsTalk">
+                    <div>
+                        <button>
+                            Let's Talk
+                        </button>
+                    </div>
+                    <div className="menu">
+                        <ul>
+                            <a href="#"><AiOutlineMenu size={25}/></a>
+                        </ul>
+                    </div>
                 </div>
                 {/* <div id="socialBar">
                     <ul> 
-                        <a href="#"><AiFillInstagram size={25}/></a>
+                        <a href="#"><AiFillInstagram size={35}/></a>
                         <a href="#"><AiFillTwitterCircle size={25}/></a>
                         <a href="#"><AiFillGithub size={25}/></a>
                         <a href="#"><AiFillProject size={25}/></a>
                     </ul>
                 </div> */}
+            </section>
+            <section className="portfolio">
+                <div>
+                    POR<span>TFO</span>LIO
+                </div>
+                <div className="description">Full stack software engineer</div>
             </section>
             <section className="svgb">
                 <SvgHomeBackground className="homeBackground" height="500px" width="500px"/>
