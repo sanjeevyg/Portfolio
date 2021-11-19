f8a0a403dc34368f431dc4a4612f42024d2f7f8b
sanjeevyg <yogi.sjv@gmail.com>
2021-11-19 02:43:32 -0500

    fSky Gazer draft layout ready

--- src/Pages/Project/Project.js
+++ src/Pages/Project/Project.js
@@ -3,62 +3,59 @@ import './index.css';
 import projectOneLogo from '../../svg/astronomy.png';
 import useMouse from '@react-hook/mouse-position';
 import { useState, useRef, useEffect } from 'react';
+import { GiYunluo } from 'react-icons/gi';
 
 export default function Project() {
 
-
-    const ref = useRef(null)
-   
-
-
-    const mouse = useMouse(ref, {
-      enterDelay: 100,
-      leaveDelay: 100,
-    })
-
-    const x = mouse.x 
-    const y = mouse.y
-
-    let xAxis = (window.innerWidth /2 - x)/25;
-    let yAxis = (window.innerWidth /2 - y)/25;
-    
-
-
-    const animateIn = (e) => {
-      e.target.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
-      console.log("animate in")
-
+    const containerOne = useRef(null)
+    const image = useRef(null)
+  
+    const handleMouseMove = (e) => {
+      console.log(e.pageX)
+      let xAxis = (window.innerWidth /2 - e.pageX)/70;
+      let yAxis = (window.innerWidth /2 - e.pageY)/70;
+      
+      containerOne.current.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
     }
 
-    const animateOut = (e) => {
-      e.target.style.transform = `rotateY(0deg) rotateX(0deg)`
-      console.log("animate out")
+    const handleMouseLeave = () => {
+      containerOne.current.style.transform = `rotateY(0deg) rotateX(0deg)`
+      containerOne.current.style.transition = 'all .9s ease'
+    }
+
+    const handleMouseEnter = () => {
+      containerOne.current.style.transition = 'none'
+      image.current.style.transform = 'translateZ(130px)'
     }
    
-   
-
     return (
-        <div classNam="projectContainer" ref={ref}>
+        <div classNam="projectContainer" >
           <section className="projectOne"> 
-            <div className="containerOne">
-                {/* onMouseEnter={animateIn} */}
-                {/* onMouseLeave={animateOut} */}
-             
-              <div className="info"> 
-                <h1 className="title">SKY GAZER</h1>
-                <h4> App built for planets</h4>
-                <button>1</button>
-                <button>2</button>
-                <button class="active">3</button>
-                <button>3</button>
-                <div className="visitWebsite"> 
-                  <button>Visit Website</button>
+            <div className="containerOne" 
+
+            ref={containerOne}
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}  
+            onMouseEnter={handleMouseEnter}  
+            >
+              <div class="cardOne" > 
+                <div className="info"> 
+                    <h1 className="title">SKY GAZER</h1>
+                    <h4> App built for planets</h4>
+                    <button>1</button>
+                    <button>2</button>
+                    <button class="active">3</button>
+                    <button>3</button>
+                    <div className="visitWebsite"> 
+                        <button>Visit Website</button>
+                    </div>
                 </div>
-              </div>
-              <div class="cardOne"> 
-                <div className="skyGazer" >
-                    <img src={projectOneLogo} alt="skyGazer" height="400" width="400"/>
+                <div className="skyGazer" ref={image}  >
+                    <img src={projectOneLogo} alt="skyGazer" />
+                    
                 </div>
+                <div className="circle"></div>
+                
               </div>
               
            </div>
