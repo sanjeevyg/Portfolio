d814cf8fc7fd0c0b04d209cf048c6993d5bf7d30
sanjeevyg <yogi.sjv@gmail.com>
2022-01-01 22:13:30 -0500

    about me section updated

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -36,10 +36,11 @@ export default function About() {
                     <div id="introduction"> 
                         <div id="aboutMe">About me</div>
                         <div id="bio"> 
-                           Welcome to my website! My name is Sanjeev Yogi. I have previously worked as an architect for a few years and construction estimator for a couple of years following my respective bachelor 
-                           degree in architecture and masters in Construction Management. Couple of years ago, I started coding as a hobby. During this period, I fell in love with it. I found many similarities between
-                            architecture and software engineering in many ways in terms of principles of aesthetics and workflow. There is more technical aspects to the software engineering.
-
+                           Welcome to my website! My name is Sanjeev Yogi. Before I started coding, I worked as an architect for a few years and construction estimator for a couple of years after my undergraduate and graduate school. 
+                           I started coding as a hobby. As I started getting my feet wet, I stumbled across several youtube videos and articles on software engineering. There is so much resources on the inernet that sometimes it is confusing and overwhelming. The learning can be
+                        be slow and painful without proper guidance. So, after much though and planning, I joined the Flatiron School in Denver in 2020 and graduated at the end of the year. I throughly enjoyed my time at Flatiron and learned 
+                        a lot. I was very eager to begin my new career but had to be away to fulfill my military duties. Now, when I am back I am more motivated than ever and ready to kickoff my software engineering journey.
+       
                         </div>
                     </div>
                     <div id="copyright"> 
