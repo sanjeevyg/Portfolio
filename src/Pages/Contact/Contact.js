e2d2fc154e644fd50f93bc438799796f377e60ee
sanjeevyg <yogi.sjv@gmail.com>
2022-01-02 22:19:58 -0500

    form height test

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -30,51 +30,53 @@ export default function Contact() {
 
     return (
         <div className= {chatBoxStatus ? "contact": "contact openBox"}>
+         <div className="contactContainer">
             <div className='btnContainer'>
                 <button id="closeBtn" onClick={closeChatBox}>Close <IoClose id="btnIcon" size={17} /></button>
             </div>
             <div className="form-container">
             <div id="letsTalk"><IoMdChatbubbles color='blue' id="chatIcon" size={50}/>Let's talk !</div>
             <div id="messageInstruction">Type your message below or send me an email at <span>syogifse@gmail.com</span>.</div>
-            <form className="contact-form" ref={form} onSubmit={sendEmail} >
-                <div className="userInfo">
-                    <label htmlFor="formName">Name</label>
-                    <input 
-                        id="formName" 
-                        type="text" 
-                        name="name"
-                        placeholder="What's your name?"
-                    />
-                </div>
+                <form className="contact-form" ref={form} onSubmit={sendEmail} >
+                    <div className="userInfo">
+                        <label htmlFor="formName">Name</label>
+                        <input 
+                            id="formName" 
+                            type="text" 
+                            name="name"
+                            placeholder="What's your name?"
+                        />
+                    </div>
 
-                <div className="userInfo">
-                    <label htmlFor="email">Email</label>
-                    <input 
-                        id="email" 
-                        type="text" 
-                        name="email"
-                        placeholder="What's your email?"
-                    />
-                </div>
+                    <div className="userInfo">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            id="email" 
+                            type="text" 
+                            name="email"
+                            placeholder="What's your email?"
+                        />
+                    </div>
 
-                <div className="userInfo messageInfo">
-                    <label htmlFor="message">Message</label>
-                    <textarea 
-                        id="message" 
-                        type="text" 
-                        name="message"
-                        rows="14"
-                         cols="10" 
-                         wrap="soft" 
-                         maxlength="1000"
-                         overflow="hidden"
-                        placeholder="Write your message!"
-                    />
-                </div>
-                <div className="btnForm">
-                    <input type="submit" value="SEND"/>
-                </div>
-            </form>   
+                    <div className="userInfo messageInfo">
+                        <label htmlFor="message">Message</label>
+                        <textarea 
+                            id="message" 
+                            type="text" 
+                            name="message"
+                            rows="14"
+                            cols="10" 
+                            wrap="soft" 
+                            maxlength="1000"
+                            overflow="hidden"
+                            placeholder="Write your message!"
+                        />
+                    </div>
+                    <div className="btnForm">
+                        <input type="submit" value="SEND"/>
+                    </div>
+                </form>   
+            </div>
             </div>
         </div>
 
