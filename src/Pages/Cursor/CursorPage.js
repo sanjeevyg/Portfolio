fb1d1e7a1e85d9e45ac3bfe0e4df4f5e4099ab28
sanjeevyg <yogi.sjv@gmail.com>
2022-01-19 23:11:33 -0500

    cursor page cleaned

--- src/Pages/Cursor/CursorPage.js
+++ src/Pages/Cursor/CursorPage.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect} from 'react';
 import './index.css';
-
-
-
 
 export default function Cursor() {
 
     const cursorRef = useRef(null)
-
-
     useEffect( () => {
         document.addEventListener("mousemove", e => {
             cursorRef.current.style.top  = `${e.pageY - 12}px`
             cursorRef.current.style.left = `${e.pageX - 12}px`
         })
-
     
     }, [])
 
     const addClassName = () => {
-        // setCursorClass("upScale")
         cursorRef.current.className = "cursor upScale"
         setTimeout( () => {
             cursorRef.current.className = "cursor"
