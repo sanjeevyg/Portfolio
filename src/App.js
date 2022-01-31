5deca611d2e84f4414193bb503b9dc41ffa06068
sanjeevyg <yogi.sjv@gmail.com>
2022-01-30 23:15:35 -0500

    minor css fixes

--- src/App.js
+++ src/App.js
@@ -3,26 +3,13 @@ import Home from './Pages/Home/Home.js';
 import Project from './Pages/Project/Project.js';
 import About from './Pages/About/About.js';
 import Contact from './Pages/Contact/Contact.js';
-import { useEffect, useRef } from 'react';
-import { useSelector } from 'react-redux';
 
 
 
 function App() {
 
-  const appRef = useRef(null)
-
-  const popUPStatus = useSelector(state => state.videoPopUp)
-  console.log(popUPStatus)
-
-  useEffect(() => {
-    popUPStatus ? appRef.current.style.overflow = "hidden" : appRef.current.style.overflow = "scroll"
-    }, [popUPStatus])
-
-  
-
   return (
-    <div id="container" ref={ appRef }>
+    <div id="container">
         <Home />
         <Project/>
         <About />
